411380b523032633fd63fd2cd8d09bf951d5f132
Dylan Evans <dylan.evans@simplesalt.company>
2025-04-21 12:00:01 -0500

    try again.

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -3,3 +3,8 @@ import { PlasmicComponent } from "@plasmicapp/loader-nextjs";
 export default function CatchallPage() {
   return <PlasmicComponent component="YourRootComponent" />;
 }
+
+// Critical: Disable ISR/SSG for Cloudflare Pages + static export
+export const getStaticProps = async () => {
+  return { props: {} }; // Plain static props
+};
