dbf78ced7fc439a0706a9b808177f5b4d0b0755d
Dylan Evans <dylan.evans@simplesalt.company>
2025-04-21 12:34:02 -0500

    pls sir may I have another

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -1,10 +1,6 @@
 import { PlasmicComponent } from "@plasmicapp/loader-nextjs";
 import { PLASMIC } from "@/plasmic-init";
 
-export default function CatchallPage() {
-  return <PlasmicComponent component="YourRootComponent" />;
-}
-
 export const getStaticPaths = async () => {
   return {
     paths: [{ params: { catchall: [] } }],
