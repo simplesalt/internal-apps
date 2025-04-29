62ffc50c42c448260884af462dab784fcdfd7c9e
Dylan Evans <dylan@simple-salt.com>
2025-04-29 09:34:28 -0500

    another whack from https://github.com/plasmicapp/plasmic/blob/master/examples/nextjs-example

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     esmExternals: "loose",
     runtime: "edge",
   },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   output: process.env.CF_PAGES ? "export" : undefined,
 };
